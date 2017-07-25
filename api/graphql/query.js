e316228424ab40e157cc91e7c9db485c23117594
Edouard Menayde <scrunshes@gmail.com>
2017-07-25 12:36:12 +0200

    :sparkles: introduce me query to improve auth experience

--- api/graphql/query.js
+++ api/graphql/query.js
@@ -1,11 +1,13 @@
 import {GraphQLObjectType} from 'graphql';
 import servicesForUser from './queries/servicesForUser';
 import services from './queries/services';
+import me from './queries/me';
 
 export default new GraphQLObjectType({
   name  : 'Query',
   fields: () => ({
     servicesForUser,
     services,
+    me,
   }),
 });
